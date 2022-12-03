10f96bc5eabdb1c584a73eac855174aab52f70fa
Henrique Spadim <henrique@spadim.com.br>
2022-12-02 21:20:09 -0300

    Add finished day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,7 +1,7 @@
 const _DUMMY_INPUT: &str = include_str!("data/day2-dummy.txt");
 const REAL_INPUT: &str = include_str!("data/day2-real.txt");
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Copy, Debug)]
 enum Jokenpo {
     Rock,
     Paper,
@@ -44,6 +44,19 @@ impl From<&str> for Jokenpo {
     }
 }
 
+impl From<i32> for Jokenpo {
+    fn from(jokenpo: i32) -> Self {
+        match jokenpo {
+            // From Modular Arithmetic
+            0 => Self::Rock,
+            1 => Self::Paper,
+            2 => Self::Scissors,
+            // Invalid
+            _ => Self::Invalid,
+        }
+    }
+}
+
 impl Jokenpo {
     const fn value(&self) -> i32 {
         match self {
@@ -55,19 +68,53 @@ impl Jokenpo {
     }
 }
 
-// enum MatchResult {
-//     Win,  // 6
-//     Draw, // 3
-//     Lost, // 0
-// }
+enum MatchResult {
+    Win,  // 6
+    Draw, // 3
+    Lose, // 0
+    Invalid,
+}
+
+impl From<&str> for MatchResult {
+    fn from(match_result: &str) -> Self {
+        match match_result {
+            // Player
+            "X" => Self::Lose,
+            "Y" => Self::Draw,
+            "Z" => Self::Win,
+            // Invalid
+            _ => Self::Invalid,
+        }
+    }
+}
+
+impl MatchResult {
+    const fn value(&self) -> i32 {
+        match self {
+            Self::Lose => 0,
+            Self::Draw => 3,
+            Self::Win => 6,
+            Self::Invalid => 0,
+        }
+    }
+}
 
 fn eval_match_result(jokenpo_player: Jokenpo, jokenpo_opponent: Jokenpo) -> i32 {
     if jokenpo_player == jokenpo_opponent {
-        3 + jokenpo_player.value() // Draw
+        MatchResult::Draw.value() + jokenpo_player.value() // Draw
     } else if (jokenpo_player.value() % 3) == ((jokenpo_opponent.value() + 1) % 3) {
-        6 + jokenpo_player.value() // Win (player won against the opponent)
+        MatchResult::Win.value() + jokenpo_player.value() // Win (player won against the opponent)
     } else {
-        0 + jokenpo_player.value() // Lost (player lost against the opponent)
+        MatchResult::Lose.value() + jokenpo_player.value() // Lose (player lost against the opponent)
+    }
+}
+
+fn eval_opponent_hand(jokenpo_opponent: Jokenpo, match_result: MatchResult) -> Jokenpo {
+    match match_result {
+        MatchResult::Draw => jokenpo_opponent,
+        MatchResult::Win => Jokenpo::from(jokenpo_opponent.value() % 3),
+        MatchResult::Lose => Jokenpo::from((jokenpo_opponent.value() + 1) % 3),
+        MatchResult::Invalid => Jokenpo::Invalid,
     }
 }
 
@@ -85,7 +132,17 @@ fn private_solve_part_1(values: &str) -> String {
 }
 
 fn private_solve_part_2(values: &str) -> String {
-    unimplemented!()
+    values
+        .lines()
+        .map(|line| {
+            let mut iter = line.split_ascii_whitespace();
+            let jokenpo_opponent = Jokenpo::from(iter.next().unwrap());
+            let match_result = MatchResult::from(iter.next().unwrap());
+            let jokenpo_player = eval_opponent_hand(jokenpo_opponent, match_result);
+            eval_match_result(jokenpo_player, jokenpo_opponent)
+        })
+        .sum::<i32>()
+        .to_string()
 }
 
 fn _solve_part_1_dummy() -> String {
@@ -107,9 +164,13 @@ pub fn solve_part_2_real() -> String {
 #[cfg(test)]
 mod tests {
     use super::{
-        _solve_part_1_dummy, _solve_part_2_dummy, eval_match_result, solve_part_1_real,
-        solve_part_2_real, Jokenpo,
+        _solve_part_1_dummy, _solve_part_2_dummy, eval_match_result, eval_opponent_hand,
+        solve_part_1_real, solve_part_2_real, Jokenpo, MatchResult,
     };
+
+    ///////////////////////
+    // Common Evaluation //
+    ///////////////////////
 
     #[test]
     fn test_logic_when_draw() {
@@ -159,13 +220,67 @@ mod tests {
         );
     }
 
+    //////////////////////
+    // Match Evaluation //
+    //////////////////////
+
+    #[test]
+    fn test_opponent_hand_logic_when_draw() {
+        assert_eq!(
+            eval_opponent_hand(Jokenpo::Paper, MatchResult::Draw),
+            Jokenpo::Paper
+        );
+        assert_eq!(
+            eval_opponent_hand(Jokenpo::Rock, MatchResult::Draw),
+            Jokenpo::Rock
+        );
+        assert_eq!(
+            eval_opponent_hand(Jokenpo::Scissors, MatchResult::Draw),
+            Jokenpo::Scissors
+        );
+    }
+
+    #[test]
+    fn test_opponent_hand_logic_when_lose() {
+        assert_eq!(
+            eval_opponent_hand(Jokenpo::Paper, MatchResult::Lose),
+            Jokenpo::Rock
+        );
+        assert_eq!(
+            eval_opponent_hand(Jokenpo::Rock, MatchResult::Lose),
+            Jokenpo::Scissors
+        );
+        assert_eq!(
+            eval_opponent_hand(Jokenpo::Scissors, MatchResult::Lose),
+            Jokenpo::Paper
+        );
+    }
+
+    #[test]
+    fn test_opponent_hand_logic_when_win() {
+        assert_eq!(
+            eval_opponent_hand(Jokenpo::Paper, MatchResult::Win),
+            Jokenpo::Scissors
+        );
+        assert_eq!(
+            eval_opponent_hand(Jokenpo::Rock, MatchResult::Win),
+            Jokenpo::Paper
+        );
+        assert_eq!(
+            eval_opponent_hand(Jokenpo::Scissors, MatchResult::Win),
+            Jokenpo::Rock
+        );
+    }
+
+    // Day 2 results
+
     #[test]
     fn test_part_1_dummy() {
         assert_eq!("15", _solve_part_1_dummy());
     }
     #[test]
     fn test_part_2_dummy() {
-        assert_eq!("", _solve_part_2_dummy());
+        assert_eq!("12", _solve_part_2_dummy());
     }
     #[test]
     fn test_part_1_real() {
