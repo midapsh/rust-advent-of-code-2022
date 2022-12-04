e753b8f958d96776e2f39123f64694250d8cdcb1
Henrique Spadim <henrique@spadim.com.br>
2022-12-04 13:07:42 -0300

    Done Day 4 part 1

--- src/day4.rs
+++ src/day4.rs
@@ -2,7 +2,25 @@ const _DUMMY_INPUT: &str = include_str!("data/day4-dummy.txt");
 const REAL_INPUT: &str = include_str!("data/day4-real.txt");
 
 fn private_solve_part_1(values: &str) -> String {
-    unimplemented!()
+    values
+        .lines()
+        .map(|line| {
+            let (range1, range2) = line.split_once(',').unwrap();
+
+            let (start1, end1) = range1.split_once('-').unwrap();
+            let (start2, end2) = range2.split_once('-').unwrap();
+
+            let (start1, end1) = (start1.parse::<i32>().unwrap(), end1.parse::<i32>().unwrap());
+            let (start2, end2) = (start2.parse::<i32>().unwrap(), end2.parse::<i32>().unwrap());
+
+            if ((start1 <= start2) & (end2 <= end1)) | ((start2 <= start1) & (end1 <= end2)) {
+                1
+            } else {
+                0
+            }
+        })
+        .sum::<i32>()
+        .to_string()
 }
 
 fn private_solve_part_2(values: &str) -> String {
@@ -31,7 +49,7 @@ mod tests {
 
     #[test]
     fn test_part_1_dummy() {
-        assert_eq!("", _solve_part_1_dummy());
+        assert_eq!("2", _solve_part_1_dummy());
     }
     #[test]
     fn test_part_2_dummy() {
@@ -39,7 +57,7 @@ mod tests {
     }
     #[test]
     fn test_part_1_real() {
-        println!("{}", solve_part_1_real());
+        println!("{}", solve_part_1_real()); // 471
     }
     #[test]
     fn test_part_2_real() {
