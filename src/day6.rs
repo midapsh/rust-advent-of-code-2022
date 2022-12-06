39ededa528747220ff1a7af7461ecefefb620454
Henrique Spadim <henrique@spadim.com.br>
2022-12-06 07:34:10 -0300

    Add finished day 6

--- src/day6.rs
+++ src/day6.rs
@@ -2,7 +2,26 @@ const _DUMMY_INPUT: &str = include_str!("data/day6-dummy.txt");
 const REAL_INPUT: &str = include_str!("data/day6-real.txt");
 
 fn private_solve_part_1(values: &str) -> String {
-    unimplemented!()
+    let line_chars = values.lines().next().unwrap().chars();
+
+    let mut start_of_packet_marker: Vec<char> = vec![];
+
+    for (pos, letter) in line_chars.enumerate() {
+        match start_of_packet_marker.iter().position(|&c| c == letter) {
+            Some(index) => {
+                start_of_packet_marker.drain(..=index);
+                start_of_packet_marker.push(letter);
+            },
+            None => {
+                start_of_packet_marker.push(letter);
+                if start_of_packet_marker.len() == 4 {
+                    return (pos + 1).to_string();
+                }
+            },
+        }
+    }
+
+    0.to_string()
 }
 
 fn private_solve_part_2(values: &str) -> String {
@@ -31,7 +50,7 @@ mod tests {
 
     #[test]
     fn test_part_1_dummy() {
-        assert_eq!("", _solve_part_1_dummy());
+        assert_eq!("7", _solve_part_1_dummy());
     }
     #[test]
     fn test_part_2_dummy() {
@@ -39,7 +58,7 @@ mod tests {
     }
     #[test]
     fn test_part_1_real() {
-        println!("{}", solve_part_1_real());
+        println!("{}", solve_part_1_real()); // 1953
     }
     #[test]
     fn test_part_2_real() {
